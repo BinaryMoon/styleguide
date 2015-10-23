20f345cf68d5ee30655a6e5d6f1037e29f835b39
BinaryMoon <binarymoon@gmail.com>
2015-10-23 16:34:23 +0100

    fix a few small bugs in monet

--- theme-styles/monet.php
+++ theme-styles/monet.php
@@ -22,49 +22,56 @@ $css = <<<CSS
 	blockquote {
 		border-color: {{color-key-bg-0}};
 	}
+	#minor-sidebar .sidebar-footer-toggle a:before {
+		border-color:{{color-key-fg-0}};
+		color: {{color-key-fg-0}};
+	}
+	#minor-sidebar,
 	.masthead {
 		background-color: {{color-key-bg-0}};
 		color: {{color-key-fg-0}};
 	}
-	.masthead a {
+	#minor-sidebar .menu-social-links a:before,
+	.masthead .menu a {
 		color: {{color-key-fg-0}};
+		border-color:{{color-key-fg-0}};
 	}
-	{
-		background-color: {{color-key-bg-2}};
-		color: {{color-key-fg-2}};
+	.masthead .menu ul a.menu-back:before,
+	.masthead .menu ul a.menu-expand:before {
+		border-left-color: {{color-key-fg-0}};
 	}
 	.main .pagination span.current {
 		background-color: {{color-theme-background-bg-2}};
 		color: {{color-theme-background-fg-2}};
 	}
-		background-color: {{color-key-bg-0}};
-		color: {{color-key-fg-0}};
-	}
 CSS;
 
-add_theme_support( 'styleguide', array(
-	'colors' => array(
-		'key' => array(
-			'label' => __( 'Key Color', 'styleguide' ),
-			'default' => '#ffffff',
+add_theme_support(
+	'styleguide',
+	array(
+		'colors' => array(
+			'key' => array(
+				'label' => __( 'Key Color', 'styleguide' ),
+				'default' => '#ffffff',
+			),
+			'link' => array(
+				'label' => __( 'Link Color', 'styleguide' ),
+				'default' => '#7f7f7f',
+			),
 		),
-		'link' => array(
-			'label' => __( 'Link Color', 'styleguide' ),
-			'default' => '#7f7f7f',
+		'fonts' => array(
+			'headers' => array(
+				'label' => __( 'Header Font', 'styleguide' ),
+				'default' => 'Amiri',
+			),
+			'body' => array(
+				'label' => __( 'Body Font', 'styleguide' ),
+				'default' => 'Open+Sans',
+			),
 		),
-	),
-	'fonts' => array(
-		'headers' => array(
-			'label' => __( 'Header Font', 'styleguide' ),
-			'default' => 'Amiri',
+		'css' => $css,
+		'dequeue' => array(
+			'monet-fonts',
 		),
-		'body' => array(
-			'label' => __( 'Body Font', 'styleguide' ),
-			'default' => 'Open+Sans',
-		),
-	),
-	'css' => $css,
-	'dequeue' => array(
-		'monet-fonts',
-	),
-) );
+	)
+);
